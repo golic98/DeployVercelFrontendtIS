f513961bab81472a5670007a45ae6ebd392134e7
golic98 <00214917@uca.edu.sv>
2025-10-29 13:42:03 -0600

    actualizando baseURL de axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-    baseURL: import.meta.env.VITE_APP_API_URL,
+    baseURL: 'https://service-project-theta.vercel.app/api',
     withCredentials: true,
     timeout: 10000
 });
