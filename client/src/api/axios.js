f06629519bcecb6f7efeadd1cdb4d506c7b2754c
golic98 <00214917@uca.edu.sv>
2025-10-31 11:06:34 -0600

    actualizar authcontext

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-    baseURL: 'https://deploy-vercel-backend-is.vercel.app/api',
+    baseURL: '/api',
     withCredentials: true,
     timeout: 10000
 });
