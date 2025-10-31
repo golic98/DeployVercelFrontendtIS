948696aa2a7b9a828472b781454164ae6e8213d1
golic98 <00214917@uca.edu.sv>
2025-10-31 11:43:22 -0600

    actualizando titulo de index y agregando console en authcontext

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -79,6 +79,7 @@ export const AuthProvider = ({ children }) => {
   };
 
   const logout = () => {
+    console.log("Borrando cookie");
     Cookies.remove("token");
     setIsAuthenticate(false);
     setUser(null);
